cdce96c1f4d4f035b7cde8d8baa01ba9df2507a7
Alex Zhang <alexgzhang92@gmail.com>
2018-05-27 20:29:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-
+def prime?(integer)
+  if integer == 2
+    return true
+  elsif integer % integer == 0 && integer / 1 == integer && integer % 2 == 1
+    return true
+  else
+    return false
+  end
+end
