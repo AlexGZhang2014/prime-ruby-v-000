5749671660a392b02ab0825b9f28e9ad5092f2d7
Alex Zhang <alexgzhang92@gmail.com>
2018-05-27 20:39:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   array = (2..integer-1).to_a
   if integer == 2
     return true
-  elsif integer > 1 && integer % integer == 0 && integer / 1 == integer && array.none? { |num| integer % num == 0 }
+  elsif array.none? { |num| integer % num == 0 }
     return true
   else
     return false
